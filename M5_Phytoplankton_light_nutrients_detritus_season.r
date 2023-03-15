ea809e9f327179aa8002a147d2b16273f3d8b091
EmilBohr <s190712@student.dtu.dk>
2023-03-15 16:38:21 +0100

    Update M5_Phytoplankton_light_nutrients_detritus_season.r

--- M5_Phytoplankton_light_nutrients_detritus_season.r
+++ M5_Phytoplankton_light_nutrients_detritus_season.r
@@ -22,6 +22,7 @@ param$Nb <- 30 # nutrient content at the bottom (mmol N/m^3)
 param$dz <- 2.5 # grid spacing (m)
 param$z <- seq(param$dz/2,100,by = param$dz) # depth (m)
 param$n <- length(param$z) # number of grid cells
+pee <- 555
 
 # Define initial conditions - concentrations of phytoplankton and nutrients
 PND <- c(rep(10,param$n),rep(param$Nb, param$n),rep(0,param$n))
